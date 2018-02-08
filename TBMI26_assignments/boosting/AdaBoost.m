4149a1bba0e98dbe583a109b48fff67364325e96
Johan Edstedt <johed950@student.liu.se>
2018-02-08 15:49:55 +0100

    Adaboost seems to be working now

--- TBMI26_assignments/boosting/AdaBoost.m
+++ TBMI26_assignments/boosting/AdaBoost.m
@@ -21,7 +21,7 @@ for k=1:25
 end
 
 % Generate Haar feature masks
-nbrHaarFeatures = 25;
+nbrHaarFeatures = 30;
 haarFeatureMasks = GenerateHaarFeatureMasks(nbrHaarFeatures);
 
 figure(3);
@@ -34,7 +34,7 @@ end
 
 % Create a training data set with a number of training data examples
 % from each class. Non-faces = class label y=-1, faces = class label y=1
-nbrTrainExamples = 50;
+nbrTrainExamples = 30;
 trainImages = cat(3,faces(:,:,1:nbrTrainExamples),nonfaces(:,:,1:nbrTrainExamples));
 xTrain = ExtractHaarFeatures(trainImages,haarFeatureMasks);
 yTrain = [ones(1,nbrTrainExamples), -ones(1,nbrTrainExamples)];
@@ -43,26 +43,27 @@ yTrain = [ones(1,nbrTrainExamples), -ones(1,nbrTrainExamples)];
 %  Use your implementation of WeakClassifier and WeakClassifierError
 n = size(xTrain, 2);
 d = ones(1, n) / n;
-m = 50;
+m = 8;
 ts = zeros(m, 1);
 ps = zeros(m, 1);
-ks = zeros(m, 1);
+fs = zeros(m, 1);
 as = zeros(m, 1);
 
 %%
 for i = 1:m
+    %disp(i)
     e_min = inf;
     t_min = 0;
     p_min = 0;
-    k_min = 0;
+    f_min = 0;
     a_min = 0;
-    disp('New Classifier')
-    for k = 1:size(xTrain, 1)
-        thresholds = xTrain(k,:) + 0.01;
-        disp('New Feature')
+    %disp('New Classifier')
+    for f = 1:size(xTrain, 1)
+        thresholds = xTrain(f,:) + 0.01;
+        %disp('New Feature')
         for t = thresholds
             p   = 1;
-            h   = WeakClassifier(t, p, xTrain(k,:));
+            h   = WeakClassifier(t, p, xTrain(f,:));
             e_t = WeakClassifierError(h, d, yTrain);
 
             if e_t > 0.5 
@@ -71,12 +72,13 @@ for i = 1:m
             end
 
             if e_t < e_min
-                e_min = e_t
+                e_min = e_t;
                 a = log((1 - e_min) / e_min) / 2;
                 t_min = t;
                 p_min = p;
-                k_min = k;
+                f_min = f;
                 a_min = a;
+                h_min = p*h;
             end
         end    
     end
@@ -85,36 +87,16 @@ for i = 1:m
         break;
     end
         
-    d = exp(-a * yTrain .* h);
+    d = d.*exp(-a_min * yTrain .* h_min); %Vi hade glömt att spara tidigare d -_-
+    d(d>0.5) = 0.5;
+    %disp(d)
+    %disp('Sum d')
+    %disp(sum(d))
     d = d ./ sum(d);
     ts(i) = t_min;
     ps(i) = p_min;
-    ks(i) = k_min;
+    fs(i) = f_min;
     as(i) = a_min;
-end
-%%
-k = 25;
-e_min = inf
-for t = thresholds
-    p   = 1;
-    h   = WeakClassifier(t, p, xTrain(k,:));
-    e_t = WeakClassifierError(h, d, yTrain);
-    
-    if e_t > 0.5
-        p = -p;
-        e_t = 1 - e_t;
-    end
-    
-    if e_t < e_min
-        e_min = e_t
-        a = log((1 - e_min) / e_min) / 2;
-        h
-        yTrain
-        t_min = t
-        p_min = p;
-        k_min = k;
-        a_min = a;
-    end
 end
 %% Extract test data
 
@@ -128,15 +110,18 @@ yTest = [ones(1,nbrTestExamples), -ones(1,nbrTestExamples)];
 %  You can evaluate on the training data if you want, but you CANNOT use
 %  this as a performance metric since it is biased. You MUST use the test
 %  data to truly evaluate the strong classifier.
-
-cs = zeros(length(ts), n);
-for i = 1:length(cs)
-    i
-    cs(i,:) = as(i) .* WeakClassifier(ts(i), ps(i), yTrain(ks(i),:));
+n_test = size(yTest,2);
+cs = zeros(length(ts), n_test);
+for i = 1:size(cs,1)
+    size(WeakClassifier(ts(i), ps(i), xTest(fs(i),:)))
+    size(as(i))
+    cs(i,:) = as(i) * WeakClassifier(ts(i), ps(i), xTest(fs(i),:));
 end
  
-sign(sum(cs))
+Classifications = sign(sum(cs,1));
 
+%%
+Accuracy = 1 - mean(abs(Classifications - yTest))/2
 %% Plot the error of the strong classifier as  function of the number of weak classifiers.
 %  Note: you can find this error without re-training with a different
 %  number of weak classifiers.
