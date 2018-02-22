16c3ef31ea76271d5c9562be6f6602160dad1821
SebastianCallh <sebastian.callh@gmail.com>
2018-02-22 18:41:17 +0100

    lab 4 with some questions remaining

--- TBMI26_assignments/Qlearning/main.m
+++ TBMI26_assignments/Qlearning/main.m
@@ -1,28 +1,38 @@
 
 
-nEpisodes = 100000;
-goalReward = 2000;
-actions = [1, 2, 3, 4];
+
+level = 4;                     % Values 1,2,3,4 for different levels
+nEpisodes = 10000;             % Amount of episodes
+gamma = 0.9;%0.3;                   % Discount factor
+initAlpha = 0.2;%0.9;               % Confidence
+initEpsilon = 0.8;%0.9;             % Prob. of random
+actions = [1, 2, 3, 4];        % All actions
+probs = [1/4, 1/4, 1/4, 1/4];  % Distribution over actions
+
+gwinit(level);
+state = gwstate();
 Q = randn(state.xsize, state.ysize, size(actions, 2));
-
-alpha = 0.9;   % Confidence
-gamma = 0.2;   % Discount factor
-initEpsilon = 0.9; % Prob. of random
-probs = [1/4, 1/4, 1/4, 1/4];
-
+Q(1,:,2)   = -inf;
+Q(end,:,1) = -inf;
+Q(:,1,4)   = -inf;
+Q(:,end,3) = -inf;
+tic
 for n = 1:nEpisodes
     if ~mod(n, 100)
+        toc
+        tic
         n
     end
     
-    if (n > nEpisodes * 0.9)
-        gwplotarrow([x,y],a);
-        gwdraw();
-    end
+    %if (n > nEpisodes * 0.9)
+    %    gwplotarrow([x,y],a);
+    %    gwdraw();
+    %end
         
-    gwinit(1);
+    gwinit(level);
     state = gwstate();
-    epsilon = initEpsilon*(exp(-n/(nEpisodes*0.2)));
+    epsilon = initEpsilon*(exp(-n/(nEpisodes*1)));
+    %alpha   = initAlpha*(exp(-n/(nEpisodes*0.5)));
     while ~state.isterminal
         x = state.pos(1);
         y = state.pos(2);
@@ -31,27 +41,35 @@ for n = 1:nEpisodes
             
             [a, ~] = chooseaction(Q, x, y, ...
                               actions, probs, epsilon);
+                                        
             gwaction(a);
             state = gwstate();
             if state.isvalid
                 break;
             end
             
-            % state is invalid
-            Q(state.pos(1), state.pos(2), :) = -inf * ones(size(actions));           
+            % Action was not valid
+            %Q(x, y, a) = -inf; 
         end
         
+        r = state.feedback;
         x_prime = state.pos(1);
         y_prime = state.pos(2);
         [~, oa] = chooseaction(Q, x_prime, y_prime, ...
                               actions, probs, epsilon);
-        r = 0;
-        if state.isterminal
-            %Q(x_prime, y_prime, :) = goalReward*[1, 1, 1, 1];
-            r = goalReward;
-        end
+        
         
         Q(x, y, a) = (1 - alpha)*Q(x, y, a) + ...
                         alpha*(r + gamma*Q(x_prime, y_prime, oa));
     end
 end
+toc
+%%
+%Q = randn(state.xsize, state.ysize, size(actions, 2));
+gwdraw();
+gwplotallarrows(Q,actions,probs);
+
+%%
+gwinit(3);
+gwdraw();
+gwplotallarrows(Q,actions,probs);
